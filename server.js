c8475bf14cbb9fb835e346f4eda535d056041b05
RealTakoChannel <873100637@qq.com>
2025-05-17 06:42:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
+
 const loginAPI = require('./controllerAPI/account-api');
 const adminAPI = require('./controllerAPI/admin-api')
 const userAPI = require('./controllerAPI/user-api')
@@ -12,7 +14,15 @@ const songAPI = require('./controllerAPI/song-api')
 const aiAPI = require('./controllerAPI/ai-api')
 
 const app = express();
+
 app.use(cors({origin: '*'}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -24,12 +34,6 @@ app.use('/api/comments', commentAPI);
 app.use('/api/songs', songAPI);
 app.use('/api/chat', aiAPI);
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-})
 
 
 
